67c133013f0ad1e3e9cc552ffbec1b7c5272f52c
Ramiro Lopez <Ramiro@Ramiros-MacBook-Pro.local>
2016-11-26 01:46:21 -0800

    CleanUp

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,10 +13,10 @@ function athleteButtonClicked() {
 
 function submitButtonClicked() {
     var userInput = $('#athlete-input').val();
-    
-  	if (userInput){
-  		$('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
-  	}
+
+    if (userInput) {
+        $('#athlete-buttons').append("<button type='button' onclick='searchGif(\"" + userInput + "\")' class='btn btn-primary' value=' " + userInput + "'> " + userInput + " </button>");
+    }
 }
 
 function searchGif(gifName) {
@@ -32,14 +32,14 @@ function searchGif(gifName) {
 function displayGif(response) {
     $('#athletes').empty();
     for (var i = 0; i < response.data.length; i++) {
-    	var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
-        var image = rating+'<img src= " ' + response.data[i].images.fixed_height_still.url +
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
             '" data-still=" ' + response.data[i].images.fixed_height_still.url +
             ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
 
-        image =  '<div class="col-md-4">' + image + "</div>";  
+        image = '<div class="col-md-4">' + image + "</div>";
         $('#athletes').append(image);
-}
+    }
 
     $('.movImage').on('click', function() {
         var state = $(this).attr('data-state');
